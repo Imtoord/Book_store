1443f38c0da0879cdaa11122621bd0c9ab60701c
Tarek-3ziz <tarek895646@gmail.com>
2024-04-30 19:08:23 +0300

    first tesk v.1

--- app.js
+++ app.js
@@ -1,45 +1,48 @@
-const morgan = require("morgan"); 
-require('dotenv').config();
-const express = require('express');
+const morgan = require("morgan");
+require("dotenv").config();
+const express = require("express");
 const app = express();
 
-const connectToDB = require('./config/db');
-const { ErrorHandler } = require('./utils/errorHandler');
-const { globalError } = require('./middlewares/errorMiddleware');
+const connectToDB = require("./config/db");
+const { ErrorHandler } = require("./utils/errorHandler");
+const { globalError } = require("./middlewares/errorMiddleware");
 
 // connect with DB // next( new ErrorHandler(mge, statuscoed) )
-connectToDB()
+connectToDB();
 
 // middleware
-app.use(express.json());// json ==> obj
+app.use(express.json()); // json ==> obj
 app.use(express.urlencoded({ extended: false })); // form data
-app.use("/uploads", express.static("uploads"));// image cove 
-
+app.use("/uploads", express.static("uploads")); // image cove
 
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
-// root Router 
-app.use('/api/v1', require('./routes/index'));
+app.use("/", (req, res, next) => {
+  res.status(200).json({
+    success: true,
+    message: "Hello world",
+  });
+});
 
+// root Router
+app.use("/api/v1", require("./routes/index"));
 
-app.all('*', (req, res, next) => {
-    next(
-      new ErrorHandler(
-        `Can't find ${req.method} ${req.originalUrl} on this server`,
-        404
-      )
-    );
+app.all("*", (req, res, next) => {
+  next(
+    new ErrorHandler(
+      `Can't find ${req.method} ${req.originalUrl} on this server`,
+      404
+    )
+  );
 });
 
 // global Error
-app.use(globalError) // 
+app.use(globalError); //
 
 // app prot & app listen
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening on port ${port}`))
-
-
+app.listen(port, () => console.log(`Listening on port ${port}`));
 
 // next(errr) ==> req, res, next
